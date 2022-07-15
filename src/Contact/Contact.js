df69b79160a868ed5011aa569cac35931c67d9ad
Sakurai-exe <denblaze@mail.ru>
2022-07-15 21:36:50 +0300

    Bugs with scales was fixed. Page Contact was updated. Email form was created on this page

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,9 +1,47 @@
+import React, { useRef } from "react";
 import ContactS from "./Contact.module.css";
+import emailjs from "emailjs-com";
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_8px4lda",
+        "template_nrzg1zk",
+        form.current,
+        "iDRKEkSo8yILNw9Nb"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
   return (
     <div className={ContactS.Contact}>
-      Contact me
+      <div className={ContactS.Content}>
+        <h2>Contact me</h2>
+        <p>
+          If you have other request or question, don’t <br />
+          hesitate to use the form.
+        </p>
+      </div>
+      <form ref={form} onSubmit={sendEmail}>
+        <input type="text" required placeholder="Name" name="name" />
+        <input type="email" required placeholder="Email" name="email" />
+        <input type="text" required placeholder="Subject" name="subject" />
+        <textarea required placeholder="Message" name="message" />
+        <input className={ContactS.FormButton} type="submit" value="Send" />
+      </form>
     </div>
   );
 }
